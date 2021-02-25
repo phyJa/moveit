f2959b56dc9bd6fc1f8fa41f6e4599d1636bc121
Elias Bueno Fonseca <eli.bueno9@gmail.com>
2021-02-25 11:44:54 -0300

    Add Profile component and files to the public folder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
 import { ExperienceBar } from "../Components/ExperienceBar";
+import { Profile } from "../Components/Profile";
+import styles from "../styles/pages/Home.module.css";
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className={styles.container}>
       <ExperienceBar></ExperienceBar>
+      <section>
+        <div>
+          <Profile></Profile>
+        </div>
+        <div>
+
+        </div>
+      </section>
     </div>
   );
 }
