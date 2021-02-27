be5ec76483502ebae8ba1e4878785569f7657cba
Elias Bueno Fonseca <eli.bueno9@gmail.com>
2021-02-27 12:19:08 -0300

    Challenge Box finished

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { ChallengeBox } from "../Components/ChallengeBox";
 import { CompletedChallenges } from "../Components/CompletedChallenges";
 import { Countdown } from "../Components/Countdown";
 import { ExperienceBar } from "../Components/ExperienceBar";
@@ -19,7 +20,7 @@ export default function Home() {
           <Countdown />
         </div>
         <div>
-
+          <ChallengeBox />
         </div>
       </section>
     </div>
