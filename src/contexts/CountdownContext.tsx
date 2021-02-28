031f8ad0625c3cf9ef59de8ed1373a5462d07bfc
Elias Bueno Fonseca <eli.bueno9@gmail.com>
2021-02-28 17:25:40 -0300

    Return time to 25 minutes

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -33,7 +33,7 @@ export function CountdownProvider({children}: CountdownContextProps) {
     
     // Constants
     // States
-    const [time, setTime] = useState(0.1 * 60);
+    const [time, setTime] = useState(25 * 60);
     const [isActive, setIsActive] = useState(false);
     const [hasFinished, setHasFinished] = useState(false);
     //
@@ -49,7 +49,7 @@ export function CountdownProvider({children}: CountdownContextProps) {
         clearTimeout(countdownTimeOut);
         setIsActive(false);
         setHasFinished(false);
-        setTime(0.1 * 60);
+        setTime(25 * 60);
     }
     useEffect(
         () => {
